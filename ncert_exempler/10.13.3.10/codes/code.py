13c0deca43d038b55bf5657cd019e7d72707bef5
Yash Patil <yashjpatil18@gmail.com>
2023-08-19 12:07:15 +0530

    adding 10.13.3.30

---
+++ ncert_exempler/10.13.3.10/codes/code.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Number of samples is 24, 1 denotes green marble and 0 denotes blue marble
+simlen=int(10)
+
+#choosing random number from 1 to 10
+num = np.random.randint(0,11, size=simlen)
+
+#choosing random suit
+suits = ['Diamonds', 'Hearts', 'Spades', 'Clubs']
+shape = np.random.choice(suits,size=simlen)
+
+#showing the new lines generated
+ls = eq = gr = 0
+print("Random cases generated:")
+for i in range(0,10):
+    print(num[i]," - ",shape[i])
+    if(num[i]<7):
+        ls += 1
+    elif(num[i]==7):
+        eq += 1
+    else:
+        gr += 1
+
+#printing probabilities
+print("\nProbability of:")
+print("Less than 7 - simulation, actual : ",ls/simlen,", 0.6")
+print("Equal to 7 - simulation, actual : ",eq/simlen,", 0.1")
+print("Greater than 7 - simulation, actual : ",gr/simlen,", 0.3")
